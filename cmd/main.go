583f59c83f24646b0c01d6226a1245461b0d7268
Nick Hosman <nickhosman@gmail.com>
2024-04-15 14:23:13 -0700

    added get character by ID

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -25,13 +26,31 @@ type Character struct {
 	Player User `json:"player"`
 }
 
+type Message struct {
+	Content string `json:"message"`
+}
+
+func newMessage(content string) Message {
+	return Message{
+		Content: content,
+	}
+}
+
+var Characters = make(map[int]Character)
+
 func newCharacter(id int, name string, player User) Character {
-	return Character{
+	c := Character{
 		Id: id,
 		Name: name,
 		Player: player,
 	}
+
+	Characters[c.Id] = c
+
+	return c
 }
+
+
 
 func main() {
 	e := echo.New()
@@ -47,6 +66,19 @@ func main() {
 
 	e.GET("/character", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, char)
+	})
+
+	e.GET("/character/:id", func(c echo.Context) error {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}
+
+		if Characters[id].Id > 0 {
+			return c.JSON(http.StatusOK, Characters[id])
+		}
+
+		return c.JSON(http.StatusNotFound, newMessage("Character not found."))
 	})
 
 	e.Logger.Fatal(e.Start(":4000"))
