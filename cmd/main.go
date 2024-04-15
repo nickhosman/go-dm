dcdc3a4318f853794d6c6a0f79d249ef35ccd571
Nick Hosman <nickhosman@gmail.com>
2024-04-15 13:33:15 -0700

    testing character struct

--- cmd/main.go
+++ cmd/main.go
@@ -8,12 +8,28 @@ import (
 )
 
 type User struct {
+	Id 		 int `json:"id"`
 	Username string `json:"username"`
 }
 
-func newUser(username string) User {
+func newUser(id int, username string) User {
 	return User{
+		Id: id,
 		Username: username,
+	}
+}
+
+type Character struct {
+	Id 	   int `json:"id"`
+	Name   string `json:"name"`
+	Player User `json:"player"`
+}
+
+func newCharacter(id int, name string, player User) Character {
+	return Character{
+		Id: id,
+		Name: name,
+		Player: player,
 	}
 }
 
@@ -22,10 +38,15 @@ func main() {
 
 	e.Use(middleware.Logger())
 
-	u := newUser("knocknix")
+	u := newUser(1, "knocknix")
+	char := newCharacter(1, "Drig", u)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, u)
+	})
+
+	e.GET("/character", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, char)
 	})
 
 	e.Logger.Fatal(e.Start(":4000"))
