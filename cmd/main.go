15f34c16e86202ed585155658097d67a1a50e6d7
Nick Hosman <nickhosman@gmail.com>
2024-04-17 12:07:59 -0700

    setting up db

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type User struct {
@@ -23,6 +25,7 @@ func newUser(id int, username string) User {
 type Character struct {
 	Id 	   int `json:"id"`
 	Name   string `json:"name"`
+	Lvl    int `json:"lvl"`
 	Player User `json:"player"`
 }
 
@@ -38,10 +41,11 @@ func newMessage(content string) Message {
 
 var Characters = make(map[int]Character)
 
-func newCharacter(id int, name string, player User) Character {
+func newCharacter(id int, name string, lvl int, player User) Character {
 	c := Character{
 		Id: id,
 		Name: name,
+		Lvl: lvl,
 		Player: player,
 	}
 
@@ -50,7 +54,36 @@ func newCharacter(id int, name string, player User) Character {
 	return c
 }
 
+type Feature struct {
+	Id			int `json:"id"`
+	Name 		string `json:"name"`
+	Description string `json:"description"`
+	Lvl			int `json:"lvl"`
+}
 
+func newFeature(name, description string, id, lvl int) Feature {
+	return Feature {
+		Id: id,
+		Name: name,
+		Description: description,
+		Lvl: lvl,
+	}
+}
+
+type Class struct {
+	Id 	     int `json:"id"`
+	Name     string `json:"name"`
+	Hitdie   int `json:"hitdie"`
+	Lvl 	 int `json:"lvl"`
+}
+
+func newClass(id int, name string, hitdie int, features []Feature) Class {
+	return Class {
+		Id: id,
+		Name: name,
+		Hitdie: hitdie,
+	}
+}
 
 func main() {
 	e := echo.New()
@@ -58,7 +91,7 @@ func main() {
 	e.Use(middleware.Logger())
 
 	u := newUser(1, "knocknix")
-	char := newCharacter(1, "Drig", u)
+	char := newCharacter(1, "Drig", 1, u)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, u)
@@ -82,4 +115,16 @@ func main() {
 	})
 
 	e.Logger.Fatal(e.Start(":4000"))
+}
+
+func startDB() {
+	const file string = "dm.db"
+
+	const create string = `
+	CREATE TABLE IF NOT EXISTS features (
+		id INTEGER NOT NULL PRIMARY KEY,
+		name TEXT NOT NULL,
+		description TEXT,
+		lvl INTEGER
+	);`
 }
